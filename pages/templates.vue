ebe161163e36baacbd877c6ef3934d1ce3ed7227
ERICK AYBAR <aybarhuamane@gmail.com>
2021-11-11 18:47:27 -0500

    v1.4 WIDGET MENSAJE 98

--- pages/templates.vue
+++ pages/templates.vue
@@ -1,10 +1,29 @@
 <template>
   <div>
-      <h2>
-          <iotindicator></iotindicator>
-      </h2>
+   
+   <iotindicator></iotindicator>
+   <button  @click="sendData">SEND</button>
 
   </div>
- 
+ </template>
 
-</template>
+ <script>
+  
+ export default {
+   data(){
+     return {
+       value:false
+     }
+   },
+    methods: {
+      sendData(){
+        this.value = !this.value;
+        const toSend={
+          value: this.value
+        }
+        this.$nuxt.$emit('widget-topic', toSend );
+      }
+    }
+   
+ }
+ </script>
